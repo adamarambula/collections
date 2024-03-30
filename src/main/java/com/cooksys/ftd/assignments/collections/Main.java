5becf46bf037bcbaf36ce3bbda21dcc02f40c71e
adam <adam.j.arambula@gmail.com>
2024-03-30 13:50:05 -0500

    Collections

---
+++ src/main/java/com/cooksys/ftd/assignments/collections/Main.java
@@ -0,0 +1,26 @@
+package com.cooksys.ftd.assignments.collections;
+
+import com.cooksys.ftd.assignments.collections.model.Manager;
+import com.cooksys.ftd.assignments.collections.model.OrgChart;
+import com.cooksys.ftd.assignments.collections.model.Worker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    /**
+     * TODO [OPTIONAL]: Students may use this main method to manually test their code. They can instantiate Employees
+     *  and an OrgChart here and test that the various methods they've implemented work as expected. This class and
+     *  method are purely for scratch work, and will not be graded.
+     */
+    public static void main(String[] args) {
+
+        OrgChart org = new OrgChart();
+
+        System.out.println(org.addEmployee(null));
+
+
+    }
+
+    }
---
+++ src/main/java/com/cooksys/ftd/assignments/collections/model/Worker.java
@@ -0,0 +1,117 @@
+package com.cooksys.ftd.assignments.collections.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.cooksys.ftd.assignments.collections.util.MissingImplementationException;
+
+/**
+ * TODO: Implement this class
+ *  <br><br>
+ *  A worker is a type of employee that cannot be a superior to another employee.
+ *  <br>
+ *  A worker should have a name, and, optionally, a manager superior to them.
+ */
+public class Worker implements Employee {
+
+    // TODO: Does this class need private fields? If so, add them here
+    private String name;
+    private Boolean hasManager;
+    private Manager manager;
+    /**
+     * TODO: Implement this constructor.
+     *
+     * @param name the name of the worker to be created
+     */
+    public Worker(String name) {
+        this.name = name;
+        this.hasManager = false;
+    }
+
+    /**
+     *  TODO: Implement this constructor.
+     *
+     * @param name the name of the worker to be created
+     * @param manager the direct manager of the worker to be created
+     */
+    public Worker(String name, Manager manager) {
+        this.name = name;
+        this.manager = manager;
+        this.hasManager = true;
+    }
+
+    /**
+     * TODO: Implement this getter.
+     *
+     * @return the name of the worker
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * TODO: Implement this getter.
+     *
+     * @return {@code true} if this worker has a manager, or {@code false} otherwise
+     */
+    @Override
+    public boolean hasManager() {
+        return hasManager;
+    }
+
+    /**
+     * TODO: Implement this getter.
+     *
+     * @return the manager of this worker, or null if it has none
+     */
+    @Override
+    public Manager getManager() {
+        return manager;
+    }
+
+    /**
+     * TODO: Implement this method.
+     *  <br><br>
+     *  Retrieves the worker's chain of command as a {@code List<Manager>}, starting with their direct {@code Manager},
+     *  followed by that {@code Manager}'s {@code Manager}, and so on, until the top of the hierarchy is reached.
+     *  <br><br>
+     *  The returned list should never be or contain {@code null}.
+     *  <br><br>
+     *  If the worker does not have a {@code Manager}, an empty
+     *  {@code List<Manager>} should be returned.
+     *
+     * @return a {@code List<Manager>} that represents the worker's chain of command,
+     */
+    @Override
+    public List<Manager> getChainOfCommand() {
+        List<Manager> managerList = new ArrayList<>();
+        if(manager != null){
+            Manager isManager = manager;
+            while(isManager != null){
+                managerList.add(isManager);
+                isManager = isManager.getManager();
+            }
+        }
+        return managerList;
+    }
+
+    // TODO: Does this class need custom .equals() and .hashcode() methods? If so, implement them here.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(name, worker.name) && Objects.equals(hasManager, worker.hasManager) && Objects.equals(manager, worker.manager);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, hasManager, manager);
+    }
+
+
+    // TODO [OPTIONAL]: Consider adding a custom .toString() method here if you want to debug your code with System.out.println() statements
+}
